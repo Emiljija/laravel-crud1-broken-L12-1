ce0e93983be3f17c5edb7585d99c51b47ec28c61
EmilsPT2022 <emils@gmail.com>
2026-03-24 13:46:04 +0200

    Added an about page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@foreach ($data as $prop)
+    <p>{{ $prop }}</p>
+@endforeach
+
+<a href="/books">All books</a>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/pages/about', ['data' => ['name' => 'Evita', 'age' => 22]]);
 });
 
 Route::get('/books', [BookController::class, 'index']);
